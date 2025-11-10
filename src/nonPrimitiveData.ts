eae32c38807f941e1d5bb6ef1935694e6e1c2f27
rifat-54 <rifat999por@gmail.com>
2025-11-10 21:05:01 +0600

    first commit

---
+++ src/nonPrimitiveData.ts
@@ -0,0 +1,15 @@
+
+let bazer=['abocado','banana',234]
+bazer.push('rasberry')
+
+
+let user:(string|Number)[]=['kukumba',12]
+
+let user1=['tomato','cheery']
+let num=[23,234,235]
+
+let user3:[string ,Number]=['rakib',234]
+
+user3.push(234)
+
+let des:[string,string,number]=['dhaka','chittagram',23]
---
+++ src/primitiveDataTypes.ts
@@ -0,0 +1,17 @@
+let userName='slkfj'
+userName='324';
+
+console.log(userName);
+
+let isAdmin=false;
+isAdmin=true;
+
+let x;
+x=false;
+
+let y:undefined
+
+y=undefined
+
+
+
