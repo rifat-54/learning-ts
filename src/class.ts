b42b9a1b84b398f99b086241acda12fb99018200
rifat-54 <rifat999por@gmail.com>
2025-11-18 11:24:03 +0600

    impliment class

---
+++ src/class.ts
@@ -0,0 +1,16 @@
+class Animal{
+    name:string
+    species:string
+    sound:string
+
+    constructor(name:string,species:string,sound:string){
+        this.name=name,
+        this.species=species,
+        this.sound=sound
+
+    }
+}
+
+const dog=new Animal('dogesh','dog',"ghew ghew")
+
+console.log(dog.name);
